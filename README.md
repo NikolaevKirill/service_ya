6754c6e1f77439eea7d64a67541c232fd8b00182
NikolaevKirill <nikolaev.kirill.p@gmail.com>
2023-02-12 01:54:47 +0300

    all working

--- README.md
+++ README.md
@@ -5,17 +5,28 @@ python -m venv .venv
 pip install -r requirements.txt
 python run_streamlit_app.py 
 ```
-Дополнительные пути и данные, отдаваемые по ним, указываются в файле ```run_streamlit_app.py```:
+Дополнительные пути указываются в файле ```run_streamlit_app.py```:
 ```
 extra_routes = {
-    "is_working": "True",
-    "is_working_dict": json.dumps({"is_working": True}),
+    "is_working": MyHandler1,
+    "is_working_dict": MyHandler2),
 }  # through routes only accepts bytes, unicode, and dict objects
 ```
-
-Путь к файлу основной страницы, который написан на коде streamlit, указывается в файле ```run_streamlit_app.py```:
+Данные, отдаваемые по этим путям, указываются в хэндлерах:
 ```
-main_script_path = "page.py"  # file with your streamlit code
+class MyHandler2(tornado.web.RequestHandler):
+    def get(self):
+        self.write(json.dumps({"is_working": True}))
+```
+
+Путь к файлу основной страницы (main_script_path), который написан на коде streamlit, указывается в файле ```run_streamlit_app.py```:
+```
+bootstrap.run(
+    main_script_path="page.py",
+    command_line=None,
+    args=[],
+    flag_options={},
+)
 ```
 
 Пример:
--- run_streamlit_app.py
+++ run_streamlit_app.py
@@ -1,17 +1,45 @@
 import json
 import streamlit.web.bootstrap as bootstrap
-from utils import ServerWithExtraRoutes
+from streamlit.web.server.server import Server, LOGGER, start_listening
+from streamlit import config
+import tornado.web
 
-extra_routes = {
-    "is_working": "True",
-    "is_working_dict": json.dumps({"is_working": True}),
-}  # through routes only accepts bytes, unicode, and dict objects
-main_script_path = "page.py"  # file with your streamlit code
 
-command_line = None
-args = []
-flag_options = {}
+class MyHandler1(tornado.web.RequestHandler):
+    def get(self):
+        self.write("11111111")
+
+
+class MyHandler2(tornado.web.RequestHandler):
+    def get(self):
+        self.write(json.dumps({"is_working": True}))
+
+
+class ServerWithExtraRoutes(Server):
+    extra_routes = [
+        ("/is_working", MyHandler1),
+        ("/is_working_dict", MyHandler2),
+    ]
+
+    async def start(self) -> None:
+        LOGGER.debug("Starting server...")
+
+        app = self._create_app()
+        app.add_handlers(r".*", self.extra_routes)
+
+        start_listening(app)
+
+        port = config.get_option("server.port")
+        LOGGER.debug("Server started on port %s", port)
+
+        await self._runtime.start()
+
 
 bootstrap.Server = ServerWithExtraRoutes
-bootstrap.Server.extra_routes = extra_routes
-bootstrap.run(main_script_path, command_line, args, flag_options)
+
+bootstrap.run(
+    main_script_path="page.py",
+    command_line=None,
+    args=[],
+    flag_options={},
+)
